eb46ae5dc71001afef8d1ed510ebe023aa4493c8
Your Name <you@example.com>
2022-11-15 12:39:51 +0100

    Update SCSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './scss/main.scss';
 import Header from "./components/Header";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
-    <Header/>
+      <>
+        <Header/>
+        <Contact/>
+      </>
   );
 }
 
