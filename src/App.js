3a610e8d50a2d1c6822cea139d04f80a23986eb4
Your Name <you@example.com>
2022-11-23 12:59:20 +0100

    Add gallery

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './scss/main.scss';
 import Header from "./components/Header";
+import Gallery from "./components/Gallery";
 import Contact from "./components/Contact";
 
 export default function App() {
   return (
       <>
         <Header/>
+          <Gallery/>
         <Contact/>
       </>
   );
