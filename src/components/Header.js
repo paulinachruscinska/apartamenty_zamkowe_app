3927b81bec725c603068e22f87bfa900021a65e9
Your Name <you@example.com>
2022-11-28 15:23:24 +0100

    Update description

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 export default function Header(){
     return(
         <header>
-            <div className='header__text'>
-                <h1>apartamenty zamkowe</h1>
-                <p>Wysoki standard w niesamowitym klimacie</p>
+            <div className='header__background'>
+                <div className='header__text'>
+                    <h1>apartamenty zamkowe</h1>
+                    <p>Wysoki standard w niesamowitym klimacie</p>
+                </div>
             </div>
         </header>
     )
