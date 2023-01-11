a1149646ab6518c73db09899a49e6a509b822f9a
Your Name <you@example.com>
2023-01-11 12:41:56 +0100

    Update carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,27 +1,32 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import classNames from 'classnames'
 
 
-export default function Carousel({carousel}){
-    const [currentSlide, setCurrentSlide]=useState(0)
-    const length = carousel.length
-    useEffect(()=>{
-        setTimeout(()=>nextSlide(), 3000)
-    },[currentSlide])
+export default function Carousel({ carousel }) {
+    const [currentSlide, setCurrentSlide] = useState(0)
+    useEffect(() => {
+        const interval = setInterval(() => setCurrentSlide(slide => slide + 1 < carousel.length ? slide + 1 : 0), 3000)
 
+        return () => {
+            clearInterval(interval)
+        }
+    }, [carousel.length])
 
-    const nextSlide=()=>{
-        setCurrentSlide(currentSlide === length - 1 ? 0 : currentSlide + 1)
-    }
-    return(
-    <section className='carousel'>
-        {carousel.map((element, index)=>{
-            return(
-            <div className={(index === currentSlide ? 'slide-active' : 'slide') + ' carousel__item'} key={index}>
-                {index === currentSlide && <img className='carousel__photo' src={element.image} alt={element.alt}/>}
-
+    return (
+        <div className='carousel'>
+            <div className='carousel__item'>
+                {carousel.map((element, index) => (
+                        <img
+                            className={classNames('carousel__photo', {
+                                'slide-active': index === currentSlide
+                            })
+                            }
+                            src={element.image}
+                            alt={element.alt}
+                        />
+                    )
+                )}
             </div>
-            )
-        })}
-    </section>
+        </div>
     )
 }
